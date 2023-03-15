880bc68cc9d1506999646b384d0c30922784e667
aleko00 <125136122+aleqandre@users.noreply.github.com>
2023-03-16 00:02:37 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#btn').on('click', function () {
+
+        $(".modal").fadeIn(500)
+    })
+
+
+    $('#close-Modal').on('click', function () {
+
+        $(".modal").fadeIn(500)
+    })
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
